b9afb0e605b5835ec5bbd096f586507920b4ee2b
G33kDude <GeekDudeAHK@Gmail.com>
2020-08-20 14:53:30 -0400

    Remove JIMP

--- dashboard.js
+++ dashboard.js
@@ -87,7 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector("#video-feed").src = root + "video";
 
         //start live-updating sensor data
-            update_sensor_data();
+        update_sensor_data();
     };
 
     //choose front or rear-facing camera
@@ -155,59 +155,40 @@ document.addEventListener('DOMContentLoaded', () => {
     //take snapshot
     document.querySelector("#take-snapshot").onclick = () => {
         //Get an image from the camera
-        Jimp.read(root+'photo.jpg').then(img => {
-            //Resize the image to 1024 px wide
-            img.resize(1024, Jimp.AUTO);
 
-            //Rotate the image 90 degrees
-            img.rotate(90);
+        let uri = `${root}/shot.jpg?${Math.floor(Math.random()*100000)}`;
 
-            //Generate data URI for this snapshot
-            // img.getBase64Async(img.getMIME()).then(uri => {
-            img.getBase64Async(Jimp.MIME_PNG).then(uri => {
-                //generate HTML source
-                let new_snapshot_source = `
-                <div class="col-6 mb-4">
-                    <button type="button" class="close text-danger d-none snapshot-close" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <img src="${uri}" class="img-fluid">
-                </div>
-                `;
+        //generate HTML source
+        let new_snapshot_source = `
+        <div class="col-6 mb-4">
+            <button type="button" class="close text-danger d-none snapshot-close" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <img src="${uri}" class="img-fluid">
+        </div>
+        `;
 
-                //convert HTML source into a DOM object
-                let template = document.createElement("template");
-                template.innerHTML = new_snapshot_source;
-                let new_snapshot = template.content.firstElementChild;
+        //convert HTML source into a DOM object
+        let template = document.createElement('template');
+        template.innerHTML = new_snapshot_source;
+        let new_snapshot = template.content.firstElementChild;
 
-                //show close button only on mouseover
-                new_snapshot.onmouseover = () => {
-                    new_snapshot.querySelector(".snapshot-close").classList.toggle("d-none", false);
-                };
-                new_snapshot.onmouseout = () => {
-                    new_snapshot.querySelector(".snapshot-close").classList.toggle("d-none", true);
-                };
+        //show close button only on mouseover
+        new_snapshot.onmouseover = () => {
+            new_snapshot.querySelector('.snapshot-close').classList.toggle('d-none', false);
+        };
+        new_snapshot.onmouseout = () => {
+            new_snapshot.querySelector('.snapshot-close').classList.toggle('d-none', true);
+        };
 
-                //delete snapshot when close button clicked
-                new_snapshot.querySelector(".snapshot-close").onclick = () => {
-                    new_snapshot.remove();
-                };
+        //delete snapshot when close button clicked
+        new_snapshot.querySelector('.snapshot-close').onclick = () => {
+            new_snapshot.remove();
+        };
 
-                //add new snapshot to document
-                let snapshot_area = document.querySelector("#snapshot-area");
-                snapshot_area.insertBefore(new_snapshot, snapshot_area.childNodes[0]);
-            });
-        }).catch(err => {
-            console.log(err);
-        });
-        
-        // //TODO: Do the image manipulation in a separate thread
-        // Jimp.read("test_snapshot.jpg").then(img => {
-        //     img.resize(1024, Jimp.AUTO);
-        //     img.rotate(90);
-        //     // console.log(`${img.getWidth()} x ${img.getHeight()}`);
-        //     img.getBase64Async(img.getMIME()).then(uri => {console.log(uri);});
-        // }).catch(err =>{});
+        //add new snapshot to document
+        let snapshot_area = document.querySelector('#snapshot-area');
+        snapshot_area.insertBefore(new_snapshot, snapshot_area.childNodes[0]);
     };
 
 });
